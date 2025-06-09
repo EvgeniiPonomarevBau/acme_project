3b8f902fbd4576dce7032edfc5228db7a76d2935
Evgenii Ponomarev <ponomarev.e@baucenter.ru>
2025-06-09 22:00:17 +0200

    10

--- acme_project/birthday/forms.py
+++ acme_project/birthday/forms.py
@@ -1,6 +1,8 @@
 from django import forms
+from django.core.exceptions import ValidationError
 from .models import Birthday
 
+BEATLES = {'Джон Леннон', 'Пол Маккартни', 'Джордж Харрисон', 'Ринго Старр'}
 
 class BirthdayForm(forms.ModelForm):
     class Meta:
@@ -11,3 +13,18 @@ class BirthdayForm(forms.ModelForm):
         model = Birthday
         # Указываем, что надо отобразить все поля.
         fields = '__all__' 
+    def clean_first_name(self):
+        # Получаем значение имени из словаря очищенных данных.
+        first_name = self.cleaned_data['first_name']
+        # Разбиваем полученную строку по пробелам 
+        # и возвращаем только первое имя.
+        return first_name.split()[0] 
+    def clean(self):
+        # Вызов родительского метода clean.
+        super().clean()
+        first_name = self.cleaned_data['first_name']
+        last_name = self.cleaned_data['last_name']
+        if f'{first_name} {last_name}' in BEATLES:
+            raise ValidationError(
+                'Мы тоже любим Битлз, но введите, пожалуйста, настоящее имя!'
+            )
